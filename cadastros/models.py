15b7c2d61b503a88a463f32b0742a018a36e0cc1
FranciscoGabriel <fg.14.cavalcandido@gmail.com>
2023-06-05 22:30:45 -0300

    ajuste funcionario

--- cadastros/models.py
+++ cadastros/models.py
@@ -62,3 +62,15 @@ class Compra(models.Model):
 
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=55)
+    cpf = models.CharField(max_length=14, verbose_name="CPF", unique=True)
+    telefone = models.CharField(max_length=15)
+    cargo = models.CharField(max_length=55)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Nome: {self.nome} | CPF: {self.cpf} | Telefone: {self.telefone} | Cargo: {self.cargo}"
