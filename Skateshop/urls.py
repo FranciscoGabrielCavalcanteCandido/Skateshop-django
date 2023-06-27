75042096a94951ddba1c64bf6f114ba965270666
Francisco Gabriel <fg.14.cavalcandido@gmail.com>
2023-06-26 22:31:16 -0300

    usuarios

--- Skateshop/urls.py
+++ Skateshop/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("paginas.urls")),
     path("", include("cadastros.urls")),
-    path("", include("usuarios.urls")),
+    path("login/", include("usuarios.urls")),
 ]
