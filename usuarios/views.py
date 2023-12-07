656370e70a47c2b2e1f70b6c15423ee8a8cc9317
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2023-12-07 20:08:34 -0300

    ajustes

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 # Create your views here.
 class UsuarioCreate(CreateView):
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy()
+    success_url = reverse_lazy('login')
     form_class = UsuarioForm
 
     def get_context_data(self, *args, **kwargs):
